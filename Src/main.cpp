8279985878454e953569407174b23f62c634b6df
elmage78 <elmagesmm@gmail.com>
2026-01-30 23:02:55 +0100

    up to date, gonna try and blur

--- Src/main.cpp
+++ Src/main.cpp
@@ -11,7 +11,7 @@ constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
 const std::string MODEL_PATH = "../models/room.obj";
-const std::string TEXTURE_PATH = "../textures/viking_room.png";
+const std::string TEXTURE_PATH = "../textures/marble.jpg";
 // max queued frames
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
